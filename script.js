e4655af35631382faa2a629c5f2d4766e095487e
Issa Abubakry Olamilekan <issaabubakry@gmail.com>
2024-05-23 14:34:17 -0700

    created a script forloop to read the seconds

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const numberHours = document.querySelector('.number-hours');
+const barSeconds = document.querySelector('.bar-seconds');
 
 const numberElement = [];
+const barlement = [];
 
 // create number hours
 for (let i = 1; i <= 12; i++) {
@@ -10,4 +12,5 @@ for (let i = 1; i <= 12; i++) {
 }
 
 numberHours.insertAdjacentHTML('afterbegin', numberElement.join(""));
-// console.log(numberElement);  
+
+// create bar seconds
