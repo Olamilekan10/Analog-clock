07c0027f5caa0b382cf2a1b680b41111b63f7a91
Issa Abubakry Olamilekan <issaabubakry@gmail.com>
2024-05-22 23:36:09 -0700

    made changes to forloop value

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const numberHours = document.querySelector('.number-hours');
 const numberElement = [];
 
 // create number hours
-for (let i = 0; i <= 12; i++) {
+for (let i = 1; i <= 12; i++) {
     numberElement.push(
         `<span style="--index:${i}"><p>${i}</p></span>`
     ); 
 }
 
 numberHours.insertAdjacentHTML('afterbegin', numberElement.join(""));
-console.log(numberElement);
+// console.log(numberElement);  
