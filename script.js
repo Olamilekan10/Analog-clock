4be26691638894908ab1d3de8d05bd4211af85c1
Issa Abubakry Olamilekan <issaabubakry@gmail.com>
2024-05-22 21:44:39 -0700

    query selected the number hours class and uses a forloop

---
+++ script.js
@@ -0,0 +1,12 @@
+const numberHours = document.querySelector('.number-hours');
+
+const numberElement = [];
+
+// create number hours
+for (let i = 0; i <= 12; i++) {
+    numberElement.push(
+
+    );
+    console.log();
+    
+}
